51ada4335c47217440a7ea80774d186b3d85d223
Michael Lincoln <michaellincoln@m-lincolns-mbp.devices.brown.edu>
2019-04-15 19:09:34 -0400

    fixed contributions js injection

--- contributions.js
+++ contributions.js
@@ -62,9 +62,9 @@ function displayContributions1(req,res,next,sts,err,data)
 function handleContributionsUpdate(req,res,next)
 {
    // convert to numbers
-   var preTax = eval(req.body.preTax);
-   var afterTax = eval(req.body.afterTax);
-   var roth = eval(req.body.roth);
+   var preTax = JSON.parse(req.body.preTax);
+   var afterTax = JSON.parse(req.body.afterTax);
+   var roth = JSON.parse(req.body.roth);
 
    var userId = req.session.userId;
 
