07dfe9f6c6864d5308e9fb1c1669fc595ca766c1
Michael Lincoln <michaellincoln@m-lincolns-mbp.devices.brown.edu>
2019-04-15 21:36:27 -0400

    got rid of all sql query string concats

--- contributions.js
+++ contributions.js
@@ -34,8 +34,8 @@ function displayContributions0(req,res,next,sts)
 {
    var userid = req.session.userId;
 
-   var q = "SELECT * FROM Contributions WHERE userId = " + userid;
-   db.query(q,function (e1,d1) { displayContributions1(req,res,next,sts,e1,d1); } );
+   var q = "SELECT * FROM Contributions WHERE userId = ?";
+   db.query(q,[userid],function (e1,d1) { displayContributions1(req,res,next,sts,e1,d1); } );
 }
 
 
@@ -93,9 +93,8 @@ function handleContributionsUpdate(req,res,next)
 			 });
     }
 
-   var q = "UPDATE Contributions SET preTax = " + preTax + ", afterTax = " + afterTax +
-      ", roth = " + roth + " WHERE userId = " + userId;
-   db.query(q,function (e1,d1) { handleContributionsUpdate1(req,res,next,e1,d1); } );
+   var q = "UPDATE Contributions SET preTax = ?, afterTax = ?, roth = ? WHERE userId = ?";
+   db.query(q,[preTax, afterTax, roth, userId],function (e1,d1) { handleContributionsUpdate1(req,res,next,e1,d1); } );
 }
 
 
